70ef275d775716740d4bec7e3bf6d1e4d06fef44
kaway <1064839051@qq.com>
2018-09-20 18:07:26 +0800

    update/host

--- common/config.js
+++ common/config.js
@@ -5,9 +5,10 @@ var host = 'http://localhost:3334';
 if (env == 'development') {
     // configure stuff here
     // app.enable("view cache");
+    console.log('我们的host是1：' + host);
 } else if (env == 'production') {
     host = 'http://39.108.61.54:3334/'
+    console.log('我们的host是2：' + host);
 }
 
-console.log('我们的host是：' + host);
 module.exports = host;
